142124f7378122ee2b67435b04b65d5e1f798c6f
Rixium <gamerpotty@gmail.com>
2020-05-07 13:13:53 +0100

    Add the game tests project and screenmanager tests

---
+++ Mayday.Game.Tests/Screens/ScreenManagerShould.cs
@@ -0,0 +1,26 @@
+﻿using Mayday.Game.Screens;
+using NSubstitute;
+using NUnit.Framework;
+using Shouldly;
+
+namespace Mayday.Game.Tests.Screens
+{
+    [TestFixture]
+    public class ScreenManagerShould
+    {
+        
+        private const string ScreenName = "MockScreen";
+        
+        [Test]
+        public void AddScreenToDictionaryByName()
+        {
+            var screenManager = new ScreenManager();
+            var screen = Substitute.For<IScreen>();
+            screen.Name.Returns(ScreenName);
+            
+            screenManager.AddScreen(screen);
+            
+            screenManager.GetScreen(ScreenName).ShouldBe(screen);
+        }
+    }
+}
