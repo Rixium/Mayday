0e7f9d11f5a3c78576cafcbaa31461c191b93352
Rixium <gamerpotty@gmail.com>
2020-05-08 00:59:16 +0100

    add graphics for spritebatch wrap

---
+++ Mayday.Game/Graphics.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+
+namespace Mayday.Game
+{
+    public class Graphics
+    {
+
+        // We wanna be able to access this everywhere.
+        // I hate this but there should only ever be one sprite batch so.. yeh.
+        public static SpriteBatch SpriteBatch;
+
+        /// <summary>
+        /// Wrapping this so we don't have to stare at the ugly ass.
+        /// </summary>
+        public static void Begin()
+        {
+            SpriteBatch.Begin(SpriteSortMode.Deferred, 
+                null, 
+                SamplerState.PointClamp, 
+                DepthStencilState.Default, 
+                RasterizerState.CullClockwise, 
+                null, 
+                Utils.Window.ViewportMatrix);
+        }
+
+        public static void End() => SpriteBatch.End();
+        
+    }
+}
