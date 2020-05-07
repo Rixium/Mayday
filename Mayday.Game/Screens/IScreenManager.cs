4fcae51509146d8a1e24501ef0144ad0568a508e
Rixium <gamerpotty@gmail.com>
2020-05-07 13:23:56 +0100

    Moved comments from implementation to interface

--- Mayday.Game/Screens/IScreenManager.cs
+++ Mayday.Game/Screens/IScreenManager.cs
@@ -1,20 +1,46 @@
-﻿using Microsoft.Xna.Framework.Graphics;
-
-namespace Mayday.Game.Screens
+﻿namespace Mayday.Game.Screens
 {
+    
+    /// <summary>
+    /// Keep track of a set of screens, and easily switch between the screens.
+    /// </summary>
     public interface IScreenManager
     {
         
+        /// <summary>
+        /// Add a screen to the screen manager to track.
+        /// </summary>
+        /// <param name="screen">The screen to add.</param>
         void AddScreen(IScreen screen);
-
+        
+        /// <summary>
+        /// Removes a given screen from the screen manager if it exists.
+        /// </summary>
+        /// <param name="screen">The name of the screen to remove.</param>
         void RemoveScreen(IScreen screen);
 
+        /// <summary>
+        /// Set the next screen to the given screen name if it exists.
+        /// Transitions will happen as defined by the user.
+        /// </summary>
+        /// <param name="screenName">The name of the screen to transition to.</param>
         void ChangeScreen(string screenName);
 
+        /// <summary>
+        /// Returns a screen by a given screen name tracked by the screen manager if it exists.
+        /// </summary>
+        /// <param name="screenName">The screen name of the screen.</param>
+        /// <returns>The screen of the specified screen name.</returns>
         IScreen GetScreen(string screenName);
 
+        /// <summary>
+        /// Updates the active screen, or updates the transition if the next screen is set.
+        /// </summary>
         void Update();
 
+        /// <summary>
+        /// Draws the active screen, or transition if the next screen is set.
+        /// </summary>
         void Draw();
 
     }
--- Mayday.Game/Screens/ScreenManager.cs
+++ Mayday.Game/Screens/ScreenManager.cs
@@ -3,69 +3,31 @@
 namespace Mayday.Game.Screens
 {
     
-    /// <summary>
-    /// Keep track of a set of screens, and easily switch between the screens.
-    /// </summary>
+    /// <inheritdoc />
     public class ScreenManager : IScreenManager
     {
-
-        /// <summary>
-        /// Keep hold of all the screens in the screen manager, by their screen name.
-        /// </summary>
-        private Dictionary<string, IScreen> _screens;
         
-        /// <summary>
-        /// The current screen to draw and update.
-        /// </summary>
+        private readonly Dictionary<string, IScreen> _screens;
+        
         private IScreen _activeScreen;
         
-        /// <summary>
-        /// The next screen to transition to.
-        /// </summary>
         private IScreen _nextScreen;
 
-        /// <summary>
-        /// Creates a new instance of the screen manager, and initializes the screen collection.
-        /// </summary>
         public ScreenManager()
         {
             _screens = new Dictionary<string, IScreen>();
         }
         
-        /// <summary>
-        /// Add a screen to the screen manager to track.
-        /// </summary>
-        /// <param name="screen">The screen to add.</param>
         public void AddScreen(IScreen screen) => _screens.Add(screen.Name, screen);
 
-        /// <summary>
-        /// Removes a given screen from the screen manager if it exists.
-        /// </summary>
-        /// <param name="screen">The name of the screen to remove.</param>
         public void RemoveScreen(IScreen screen) => _screens.Remove(screen.Name);
-
-        /// <summary>
-        /// Set the next screen to the given screen name if it exists.
-        /// Transitions will happen as defined by the user.
-        /// </summary>
-        /// <param name="screenName">The name of the screen to transition to.</param>
+        
         public void ChangeScreen(string screenName) => _nextScreen = _screens[screenName];
 
-        /// <summary>
-        /// Returns a screen by a given screen name tracked by the screen manager if it exists.
-        /// </summary>
-        /// <param name="screenName">The screen name of the screen.</param>
-        /// <returns>The screen of the specified screen name.</returns>
         public IScreen GetScreen(string screenName) => _screens[screenName];
 
-        /// <summary>
-        /// Updates the active screen, or updates the transition if the next screen is set.
-        /// </summary>
         public void Update() => _activeScreen?.Update();
         
-        /// <summary>
-        /// Draws the active screen, or transition if the next screen is set.
-        /// </summary>
         public void Draw() => _activeScreen?.Draw();
         
     }
