324f5ce55582b1d566c38e7e99610e2853b94840
Rixium <gamerpotty@gmail.com>
2020-05-07 23:13:52 +0100

    Added time util

---
+++ Mayday.Game/Utils/Time.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace Mayday.Game.Utils
+{
+    public static class Time
+    {
+
+        /// <summary>
+        /// Get the time passed since last frame.
+        /// </summary>
+        public static float DeltaTime { get; set; }
+        
+        /// <summary>
+        /// The time scale for delta time.
+        /// </summary>
+
+        public static int TimeScale { get; set; } = 1;
+
+        public static void Update(GameTime gameTime)
+        {
+            DeltaTime = (float) gameTime.ElapsedGameTime.TotalSeconds * TimeScale;
+        }
+        
+    }
+}
