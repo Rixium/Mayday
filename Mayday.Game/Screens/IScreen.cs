7b161dc692702e4e072e666fff21ade2d537ae04
Rixium <gamerpotty@gmail.com>
2020-05-08 17:27:15 +0100

    Add background color to IScreen

--- Mayday.Game/Screens/IScreen.cs
+++ Mayday.Game/Screens/IScreen.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.Xna.Framework.Graphics;
+﻿using Microsoft.Xna.Framework;
 
 namespace Mayday.Game.Screens
 {
     public interface IScreen
     {
 
+        Color BackgroundColor { get; set; }
+        
         string Name { get; set; }
         
         void Update();
