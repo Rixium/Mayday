8631d924ca01b2c4c297a9c7a4adbd7998327e38
Rixium <gamerpotty@gmail.com>
2020-05-08 17:24:53 +0100

    Add sprite

---
+++ Mayday.Game/Graphics/Sprite.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Mayday.Game.Graphics
+{
+    public class Sprite : ISprite
+    {
+        
+        public Texture2D Texture { get; set; }
+        public Rectangle? SourceRectangle { get; set; }
+        public Vector2 Origin { get; set; }
+
+        public Sprite(Texture2D texture)
+        {
+            Texture = texture;
+            Origin = new Vector2(texture.Width / 2.0f, texture.Height / 2.0f);
+        }
+        
+    }
+}
