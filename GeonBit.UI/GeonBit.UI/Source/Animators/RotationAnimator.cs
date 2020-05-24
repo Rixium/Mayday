d503faf07e7a5400b457b5e6c950b0726886080d
Rixium <gamerpotty@gmail.com>
2020-05-24 16:13:30 +0100

    Add geonbit project

---
+++ GeonBit.UI/GeonBit.UI/Source/Animators/RotationAnimator.cs
@@ -0,0 +1,26 @@
+﻿namespace GeonBit.UI.Animators
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class RotationAnimator : IAnimator
+    {
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        public override void Update()
+        {
+            TargetEntity.Rotation += (float) UserInterface.Active.CurrGameTime.ElapsedGameTime.TotalSeconds * 0.05f;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public override void Reset()
+        {
+            TargetEntity.Rotation = 0;
+        }
+        
+    }
+}
