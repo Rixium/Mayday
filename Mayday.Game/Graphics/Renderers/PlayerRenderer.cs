39374fdfea248c4f126983d59eb3448271574cdd
Rixium <gamerpotty@gmail.com>
2020-06-01 22:08:25 +0100

    Remove border from player renderer

--- Mayday.Game/Graphics/Renderers/PlayerRenderer.cs
+++ Mayday.Game/Graphics/Renderers/PlayerRenderer.cs
@@ -30,11 +30,6 @@ namespace Mayday.Game.Graphics.Renderers
             var playerPosition = player.Position;
             var flip = player.FacingDirection < 0;
             
-            if(bodySprite != null)
-                DrawSprite(bodySprite, playerPosition, flip ? SpriteEffects.FlipHorizontally : SpriteEffects.None, true);
-            if(headSprite != null)
-                DrawSprite(headSprite, playerPosition, flip ? SpriteEffects.FlipHorizontally : SpriteEffects.None, true);
-
             if (legSprite != null)
                 DrawSprite(legSprite, playerPosition, flip ? SpriteEffects.FlipHorizontally : SpriteEffects.None);
             if(bodySprite != null)
@@ -44,46 +39,12 @@ namespace Mayday.Game.Graphics.Renderers
             
         }
         
-        private void DrawSprite(ISprite sprite, Vector2 playerPosition, SpriteEffects flip, bool isBorder = false)
+        private static void DrawSprite(ISprite sprite, Vector2 playerPosition, SpriteEffects flip)
         {
-            if (isBorder)
-                DrawAsBorder(sprite, playerPosition, flip);
-            else
-                GraphicsUtils.Instance.SpriteBatch.Draw(
-                    sprite.Texture, playerPosition, sprite.SourceRectangle, Color.White,
-                    0, Vector2.Zero, 1f, flip, 0F);
+            GraphicsUtils.Instance.SpriteBatch.Draw(
+                sprite.Texture, playerPosition, sprite.SourceRectangle, Color.White,
+                0, Vector2.Zero, 1f, flip, 0F);
         }
 
-        private void DrawAsBorder(ISprite sprite, Vector2 playerPosition, SpriteEffects flip)
-        {
-
-            var current = playerPosition + Vector2.UnitX;
-            GraphicsUtils.Instance.SpriteBatch.Draw(
-                sprite.Texture,
-                current,
-                sprite.SourceRectangle, OutlineColor,
-                0f, Vector2.Zero, 1f, flip, 0F);
-            
-            current = playerPosition - Vector2.UnitX;
-            GraphicsUtils.Instance.SpriteBatch.Draw(
-                sprite.Texture,
-                current,
-                sprite.SourceRectangle, OutlineColor,
-                0f, Vector2.Zero, 1f, flip, 0F);
-            
-            current = playerPosition + Vector2.UnitY;
-            GraphicsUtils.Instance.SpriteBatch.Draw(
-                sprite.Texture,
-                current,
-                sprite.SourceRectangle, OutlineColor,
-                0f, Vector2.Zero, 1f, flip, 0F);
-            
-            current = playerPosition - Vector2.UnitY;
-            GraphicsUtils.Instance.SpriteBatch.Draw(
-                sprite.Texture,
-                current,
-                sprite.SourceRectangle, OutlineColor,
-                0f, Vector2.Zero, 1f, flip, 0F);
-        }
     }
 }
