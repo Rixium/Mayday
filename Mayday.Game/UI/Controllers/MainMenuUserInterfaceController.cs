529519c4baf84544477efbea7e8dff55a5d2f6e0
Daniel Pottenger <gamerpotty@gmail.com>
2020-06-08 00:05:25 +0100

    Hide menu panels on creating games

--- Mayday.Game/UI/Controllers/MainMenuUserInterfaceController.cs
+++ Mayday.Game/UI/Controllers/MainMenuUserInterfaceController.cs
@@ -10,54 +10,57 @@ namespace Mayday.Game.UI.Controllers
 
         private Stack<Widget> _backStack = new Stack<Widget>();
         private Widget _currentActive;
-        
+
         public MainMenuUserInterfaceController(MainMenuUserInterface userInterface)
         {
             UserInterface = userInterface;
-            
+
             Show(UserInterface.MainPanel);
-            
+
             UserInterface.MenuSinglePlayer.Click += (sender, args) =>
                 Show(UserInterface.StartGamePanel);
 
-            UserInterface.MenuMultiPlayer.Click += (sender, args) => 
+            UserInterface.MenuMultiPlayer.Click += (sender, args) =>
                 Show(UserInterface.MultiPlayerPanel);
 
             UserInterface.MultiPlayerCreateGame.Click += (sender, args) =>
                 Show(UserInterface.CreateMultiplayerGamePanel);
 
-            UserInterface.MenuSettings.Click += (sender, args) => 
+            UserInterface.MenuSettings.Click += (sender, args) =>
                 Show(UserInterface.SettingsPanel);
-            
-            UserInterface.MenuQuit.Click += (sender, args) =>
-            {
 
-            };
-            
-            UserInterface.StartGameBack.Click += (sender, args) => 
+            UserInterface.MenuQuit.Click += (sender, args) => { };
+
+            UserInterface.StartGameBack.Click += (sender, args) =>
                 ShowLast();
-            
-            UserInterface.MultiPlayerBack.Click += (sender, args) => 
+
+            UserInterface.MultiPlayerBack.Click += (sender, args) =>
                 ShowLast();
-            
-            UserInterface.SettingsBack.Click += (sender, args) => 
+
+            UserInterface.SettingsBack.Click += (sender, args) =>
                 ShowLast();
 
             UserInterface.JoinByIpBack.Click += (sender, args) =>
                 ShowLast();
-            
-            UserInterface.MultiPlayerJoinByIp.Click += (sender, args) => 
+
+            UserInterface.MultiPlayerJoinByIp.Click += (sender, args) =>
                 Show(UserInterface.JoinByIpPanel);
 
+            UserInterface.CreateMultiplayerGameCreateGame.Click += (sender, args) =>
+                UserInterface.CreateMultiplayerGamePanel.Visible = false;
+
             UserInterface.StartGameNewGame.Click += (sender, args) =>
-                UserInterface.StartGamePanel.Enabled = false;
+                UserInterface.StartGamePanel.Visible = false;
+
+            UserInterface.JoinByIpJoin.Click += (sender, args) =>
+                UserInterface.JoinByIpPanel.Visible = false;
         }
 
         private void Show(Widget toShow)
         {
             if (toShow == null)
                 return;
-            
+
             if (_currentActive != null)
             {
                 _currentActive.Visible = false;
@@ -71,13 +74,12 @@ namespace Mayday.Game.UI.Controllers
         private void ShowLast()
         {
             if (_backStack.Count == 0) return;
-            
+
             if (_currentActive != null)
                 _currentActive.Visible = false;
-            
+
             _currentActive = _backStack.Pop();
             _currentActive.Visible = true;
         }
-        
     }
 }
