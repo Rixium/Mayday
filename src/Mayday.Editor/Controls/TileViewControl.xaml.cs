103cec1a392d4ffeca71f88428552218f0c327fc
Daniel Pottenger <gamerpotty@gmail.com>
2020-06-22 00:18:29 +0100

    Save tiles drop type

--- src/Mayday.Editor/Controls/TileViewControl.xaml.cs
+++ src/Mayday.Editor/Controls/TileViewControl.xaml.cs
@@ -5,11 +5,11 @@ using Mayday.Game.Gameplay.Data;
 
 namespace Mayday.Editor.Controls
 {
-    public partial class TileViewControl : UserControl
+    public partial class TileViewControl
     {
         public TileViewControl(string key, TileProperties tile)
         {
-            DataContext = new TileViewViewModel(key, tile, new TileLoader());
+            DataContext = new TileViewViewModel(key, tile, new TileLoader(), new ItemsLoader());
             InitializeComponent();
         }
 
--- src/Mayday.Editor/ViewModels/TileViewViewModel.cs
+++ src/Mayday.Editor/ViewModels/TileViewViewModel.cs
@@ -1,4 +1,6 @@
+using System.Collections.Generic;
 using System.ComponentModel;
+using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Windows.Input;
 using Mayday.Editor.Commands;
@@ -6,6 +8,7 @@ using Mayday.Editor.Controls;
 using Mayday.Editor.Loaders;
 using Mayday.Editor.Navigation;
 using Mayday.Game.Gameplay.Data;
+using Mayday.Game.Gameplay.Items;
 
 namespace Mayday.Editor.ViewModels
 {
@@ -16,6 +19,7 @@ namespace Mayday.Editor.ViewModels
         public string Key { get; set; }
 
         private readonly ITileLoader _tileLoader;
+        private readonly IItemsLoader _itemLoader;
 
         private ICommand _saveItemCommand;
         public ICommand SaveItemCommand => _saveItemCommand ?? new RelayCommand(SaveItem);
@@ -33,15 +37,36 @@ namespace Mayday.Editor.ViewModels
             }
         }
 
-        public TileViewViewModel(string key, TileProperties tile, ITileLoader tileLoader)
+        public IEnumerable<Item> Items => _itemLoader.Items.Values.AsEnumerable();
+        public Item SelectedItemDrop { get; set; }
+
+        public TileViewViewModel(string key, TileProperties tile, ITileLoader tileLoader, IItemsLoader itemLoader)
         {
             Key = key;
             Tile = tile;
             _tileLoader = tileLoader;
+            _itemLoader = itemLoader;
+
+            InitializeItemDropType();
+        }
+
+
+        private void InitializeItemDropType()
+        {
+            foreach (var item in Items)
+            {
+                if (item.ItemId.Equals(Tile.ItemDropType))
+                {
+                    SelectedItemDrop = item;
+                    return;
+                }
+            }
         }
 
         private void SaveItem()
         {
+            Tile.ItemDropType = SelectedItemDrop?.ItemId;
+
             var tiles = _tileLoader.Tiles;
 
             if (tiles.ContainsKey(Key))
@@ -64,7 +89,7 @@ namespace Mayday.Editor.ViewModels
             _tileLoader.SetTiles(tiles);
             _tileLoader.Save();
 
-            Navigator.ShowPage(new WorldObjectsManagerControl());
+            Navigator.ShowPage(new TilesManagerControl());
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
