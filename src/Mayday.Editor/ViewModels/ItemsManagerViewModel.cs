19eb075a405a9523b33957efd4d9bccb12691d34
Daniel Pottenger <gamerpotty@gmail.com>
2020-06-21 16:20:09 +0100

    Update style

--- src/Mayday.Editor/ViewModels/ItemsManagerViewModel.cs
+++ src/Mayday.Editor/ViewModels/ItemsManagerViewModel.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Input;
 using Mayday.Editor.Commands;
+using Mayday.Editor.Controls;
 using Mayday.Editor.Loaders;
+using Mayday.Editor.Navigation;
 using Mayday.Game.Gameplay.Items;
 
 namespace Mayday.Editor.ViewModels
@@ -20,6 +22,10 @@ namespace Mayday.Editor.ViewModels
 
         private ICommand _updateCommand;
         public ICommand UpdateCommand => _updateCommand ?? new RelayCommand(UpdateSelectedItem);
+
+        private ICommand _newCommand;
+        public ICommand NewCommand => _newCommand ?? new RelayCommand(() =>
+            Navigator.ShowPage(new ItemViewControl(new Item())));
 
         public IEnumerable<Item> Items => _itemsLoader.Items.Values.AsEnumerable();
 
